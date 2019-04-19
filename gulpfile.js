ae994d17528dee7def4f3e681cad470864a04beb
znnnnn <846179626@qq.com>
2019-04-19 10:32:59 +0800

    add: tongji.js; fix: pdf cannot generated

--- gulpfile.js
+++ gulpfile.js
@@ -1,96 +1,103 @@
-const gulp = require('gulp')
-const sass = require('gulp-sass')
-const autoprefixer = require('gulp-autoprefixer')
-const jade = require('gulp-jade')
-const copy = require('gulp-copy')
-const rimrafPromise = require('rimraf-promise')
-const ghPages = require('gulp-gh-pages')
-const fs = require('fs')
-const connect = require('gulp-connect')
-const generatePdf = require('./generate_pdf')
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const autoprefixer = require("gulp-autoprefixer");
+const jade = require("gulp-jade");
+const copy = require("gulp-copy");
+const rimrafPromise = require("rimraf-promise");
+const ghPages = require("gulp-gh-pages");
+const fs = require("fs");
+const connect = require("gulp-connect");
+const generatePdf = require("./generate_pdf");
 
-gulp.task('resume-sass', () => {
+/**
+ * 执行JS压缩
+ */
+gulp.task("minify:js", [], function(cb) {
+  gulp.src("src/js/*.js").pipe(gulp.dest("dist/js/"));
+});
+
+gulp.task("resume-sass", () => {
   gulp
-    .src('src/scss/resume.scss')
-    .pipe(sass().on('error', sass.logError))
+    .src("src/scss/resume.scss")
+    .pipe(sass().on("error", sass.logError))
     .pipe(
       autoprefixer({
-        browsers: ['last 4 versions'],
+        browsers: ["last 4 versions"],
         cascade: false
       })
     )
-    .pipe(gulp.dest('dist/css/'))
-    .pipe(connect.reload())
-})
+    .pipe(gulp.dest("dist/css/"))
+    .pipe(connect.reload());
+});
 
-gulp.task('icon-sass', () => {
+gulp.task("icon-sass", () => {
   gulp
-    .src('src/scss/iconfont.scss')
-    .pipe(sass().on('error', sass.logError))
+    .src("src/scss/iconfont.scss")
+    .pipe(sass().on("error", sass.logError))
     .pipe(
       autoprefixer({
-        browsers: ['last 4 versions'],
+        browsers: ["last 4 versions"],
         cascade: false
       })
     )
-    .pipe(gulp.dest('dist/iconfont/'))
-    .pipe(connect.reload())
-})
+    .pipe(gulp.dest("dist/iconfont/"))
+    .pipe(connect.reload());
+});
 
-gulp.task('sass:watch', () => {
-  gulp.watch('./src/scss/resume.scss', ['resume-sass'])
-  gulp.watch('./src/scss/iconfont.scss', ['icon-sass'])
-  gulp.watch('./src/scss/components/*.scss', ['resume-sass'])
-})
+gulp.task("sass:watch", () => {
+  gulp.watch("./src/scss/resume.scss", ["resume-sass"]);
+  gulp.watch("./src/scss/iconfont.scss", ["icon-sass"]);
+  gulp.watch("./src/scss/components/*.scss", ["resume-sass"]);
+});
 
-gulp.task('json2jade', () => {
-  const info = JSON.parse(fs.readFileSync('./info.json', 'utf-8'))
-  const locals = highlight(info)
+gulp.task("json2jade", () => {
+  const info = JSON.parse(fs.readFileSync("./info.json", "utf-8"));
+  const locals = highlight(info);
   gulp
-    .src('./src/jade/index.jade')
+    .src("./src/jade/index.jade")
     .pipe(
       jade({
         locals
       })
     )
-    .pipe(gulp.dest('./dist/'))
-    .pipe(connect.reload())
-})
+    .pipe(gulp.dest("./dist/"))
+    .pipe(connect.reload());
+});
 
-gulp.task('json2jade:watch', () => {
-  gulp.watch('./info.json', ['json2jade'])
-})
+gulp.task("json2jade:watch", () => {
+  gulp.watch("./info.json", ["json2jade"]);
+});
 
 function src2dist(dir) {
-  return gulp.src(`./src/${dir}/*.*`).pipe(gulp.dest(`./dist/${dir}/`))
+  return gulp.src(`./src/${dir}/*.*`).pipe(gulp.dest(`./dist/${dir}/`));
 }
 
 function highlight(locals) {
-  var locals = JSON.stringify(locals)
-  const re = /`(.+?)`/g
-  locals = locals.replace(re, '<strong>$1</strong>')
-  return JSON.parse(locals)
+  var locals = JSON.stringify(locals);
+  const re = /`(.+?)`/g;
+  locals = locals.replace(re, "<strong>$1</strong>");
+  return JSON.parse(locals);
 }
 
-gulp.task('copy', () => {
-  src2dist('iconfont')
-  src2dist('img')
-  src2dist('pdf')
-  gulp.src('./CNAME').pipe(gulp.dest('./dist'))
-})
+gulp.task("copy", () => {
+  src2dist("iconfont");
+  src2dist("img");
+  src2dist("pdf");
+  gulp.src("./CNAME").pipe(gulp.dest("./dist"));
+});
 
-gulp.task('clean', () => {
-  rimrafPromise('./dist/')
-})
+gulp.task("clean", () => {
+  rimrafPromise("./dist/");
+});
 
-gulp.task('deploy', () =>
-  gulp.src('./dist/**/*').pipe(
+gulp.task("deploy", () =>
+  gulp.src("./dist/**/*").pipe(
     ghPages({
-      remoteUrl: 'git@github.com:Lxxyx/lxxyx.github.io.git',
-      branch: 'master'
+      remoteUrl: "git@github.com:Lxxyx/lxxyx.github.io.git",
+      branch: "master"
     })
   )
-)
+);
 
 let port = 9000
 
@@ -109,9 +116,11 @@ gulp.task('webserver', () => {
 
 gulp.task('dev', ['default', 'json2jade:watch', 'sass:watch', 'webserver'])
 
-gulp.task('default', ['icon-sass', 'resume-sass', 'json2jade', 'copy'])
+gulp.task('default', ['icon-sass', 'resume-sass', 'json2jade', 'copy', 'minify:js'])
 
-gulp.task('pdf', ['set-pdf-port', 'default', 'webserver'], async () => {
+gulp.task('default-pdf', ['icon-sass', 'resume-sass', 'json2jade', 'copy'])
+
+gulp.task('pdf', ['set-pdf-port', 'default-pdf', 'webserver'], async () => {
   await generatePdf('http://localhost:9001')
   connect.serverClose()
   process.exit(0)
---
+++ src/js/tongji.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?48646143a54ef3659994a64e2fc68dbf";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
